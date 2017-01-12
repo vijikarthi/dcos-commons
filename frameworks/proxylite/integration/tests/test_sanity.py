aa1a9935b34b90e8812833e2d97aadff68d329b0
Gabriel Hartmann <gabriel@mesosphere.io>
2017-01-12 13:46:15 -0800

    Cleanup unneeded files and functions.

--- frameworks/proxylite/integration/tests/test_sanity.py
+++ frameworks/proxylite/integration/tests/test_sanity.py
@@ -6,16 +6,11 @@ import re
 import shakedown
 
 from tests.test_utils import (
-    PACKAGE_NAME,
     check_health,
-    get_marathon_config,
     get_deployment_plan,
     install,
-    marathon_api_url,
     request,
-    run_dcos_cli_cmd,
-    uninstall,
-    spin
+    uninstall
 )
 
 
@@ -26,8 +21,27 @@ def setup_module(module):
 
 
 @pytest.mark.sanity
-def test_httpd_proxy():
+def test_example():
+    request(
+        dcos.http.get,
+        '{}/example'.format(shakedown.dcos_service_url('proxylite')))
+
+
+@pytest.mark.sanity
+def test_google():
+    request(
+        dcos.http.get,
+        '{}/google'.format(shakedown.dcos_service_url('proxylite')))
+
+
+@pytest.mark.sanity
+def test_httpd():
     request(
         dcos.http.get,
         '{}/httpd'.format(shakedown.dcos_service_url('proxylite')))
+
+
+@pytest.mark.sanity
+def test_plan():
+    get_deployment_plan()
 
